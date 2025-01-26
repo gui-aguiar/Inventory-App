7617753746141e2a2ada758f3cac56cffd4a43e5
gui-aguiar <guilhermetoco@hotmail.com>
2025-01-26 03:37:19 -0300

    dockerile do app e dockercompose

--- InventoryTracker.Server/Program.cs
+++ InventoryTracker.Server/Program.cs
@@ -9,23 +9,26 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddEnvironmentVariables();
 
 var dbPath = Environment.GetEnvironmentVariable("DB_PATH") ?? "D:/data/inventory.db";
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection").Replace("__DB_PATH__", dbPath);
 var port = Environment.GetEnvironmentVariable("APP_PORT") ?? "5019";
+
+SqliteConnection connection;
+if (builder.Environment.IsDevelopment())
+{
+    connection = new SqliteConnection("DataSource=:memory:");
+    connection.Open();
+}
+else
+{   
+    connection = new SqliteConnection($"DataSource={dbPath}");
+}
+
+builder.Services.AddDbContext<InventoryTrackerDBContext>(options =>
+    options.UseSqlite(connection));
 
 builder.Services.AddControllers();
 builder.Services.AddAutoMapper(typeof(MappingProfile));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-var connection = new SqliteConnection("DataSource=:memory:");
-connection.Open();
-
-builder.Services.AddDbContext<InventoryTrackerDBContext>(options =>
-    options.UseSqlite(connection)); // Registra o contexto com a conexão compartilhada
-/*
- context.Database.OpenConnection();
-context.Database.EnsureCreated();
- */
 builder.Services.AddRepositories();
 builder.Services.AddServices();
 
@@ -46,20 +49,21 @@ using (var scope = app.Services.CreateScope())
     var dbContext = scope.ServiceProvider.GetRequiredService<InventoryTrackerDBContext>();
     dbContext.Database.EnsureCreated();
 
-    var scriptPath = "D:\\GitHubRepos\\VelozientApp\\DatabaseDocker\\populate-database.sql";
-    var sqlScript = File.ReadAllText(scriptPath);
-    using var command = connection.CreateCommand();
-    foreach (var sql in sqlScript.Split(";", StringSplitOptions.RemoveEmptyEntries))
+    if (builder.Environment.IsDevelopment())
     {
-        command.CommandText = sql;
-        command.ExecuteNonQuery();
+        var scriptPath = "..\\DatabaseDocker\\populate-database.sql";
+        var sqlScript = File.ReadAllText(scriptPath);
+        using var command = connection.CreateCommand();
+        foreach (var sql in sqlScript.Split(";", StringSplitOptions.RemoveEmptyEntries))
+        {
+            command.CommandText = sql;
+            command.ExecuteNonQuery();
+        }
     }
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
